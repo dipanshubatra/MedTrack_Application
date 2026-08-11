6818234d5df972fa4422d0cae7a7cd0820058400
AK <ad4182132@gmail.com>
2026-08-11 21:57:44 +0530

    fix: disable production demo data seeding

--- Backend/src/test/java/com/medtrack/supplier/controller/SupplierSecurityIntegrationTest.java
+++ Backend/src/test/java/com/medtrack/supplier/controller/SupplierSecurityIntegrationTest.java
@@ -34,7 +34,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(properties = {
         "eureka.client.enabled=false",
         "spring.cloud.discovery.enabled=false",
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration"
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration",
+        "app.data-initializer.enabled=true"
 })
 @AutoConfigureMockMvc
 public class SupplierSecurityIntegrationTest {
